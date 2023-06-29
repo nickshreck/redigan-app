09f65cc28722b6a551837ec01c7d048c1fefcada
Nicholas <nickshreck@gmail.com>
2023-06-29 09:21:13 +0100

    first

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import styled from "@emotion/styled";
+
+function App() {
+  const PartyText = styled.div`
+    font-size: 24px;
+  `;
+
+  return (
+    <>
+      <PartyText>The Magically Mephistophelean Garden Party</PartyText>
+    </>
+  );
+}
+
+export default App;
